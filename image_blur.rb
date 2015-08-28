0c4960eaa66511294ac76a4acbd4c06581545c2c
Tyler Pendleton <tyler.maxwell.pendleton@gmail.com>
2015-08-28 18:51:40 +0000

    Created array for blur indexes

--- image_blur.rb
+++ image_blur.rb
@@ -13,7 +13,18 @@ class Image
 	end
 
 	def blur
+		ones = []
+		blur = @data
 
+		# Find the ones
+		@data.each_index do |row|
+			@data[row].each_index do |cell|
+				if @data[row][cell] == 1
+					ones << [row, cell]
+				end
+			end
+		end
+		puts ones.inspect
 	end
 end
 
@@ -24,4 +35,4 @@ image = Image.new([
   [0, 0, 0, 0]
 ])
 
-image.output_image
+image.blur
